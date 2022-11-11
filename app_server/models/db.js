04ede9d8ca5f60bc666238c37a23f2b91d4674ed
Erendile <mstfrncvzc@gmail.com>
2022-11-11 23:54:44 +0300

    db created

---
+++ app_server/models/db.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+require("./mekansema");
+var dbURI =
+  "mongodb+srv://erendile:1234@mekanbul.64r9nzd.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+function close(msg, callback) {
+  mongoose.connection.close(() => {
+    console.log(msg);
+    callback();
+  });
+}
+process.on("SIGINT", () => {
+  close("Uygulama kapatilidi", () => {
+    process.exit(0);
+  });
+});
+mongoose.connection.on("connected", () => {
+  console.log(dbURI + "adresindeki veri tabanina baglandi");
+});
+
+mongoose.connection.on("error", () => {
+  console.log("Baglanti hatasi");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Baglanti kesilidi");
+});
